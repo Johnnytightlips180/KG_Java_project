d04971ae433370dc7454475c8e7af9cfb1f16172
GavinJudge <119045898+GavinJudge@users.noreply.github.com>
2022-11-30 16:48:36 +0000

    Add files via upload

---
+++ Bike_shop_application.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bike_shop_application;
+
+/**
+ *
+ * @author user
+ */
+public class Bike_shop_application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
